a40d28c1f5a9c24c54d897bddaf28e49ebcafb76
J-2001 <roldangomezjuan01@gmail.com>
2024-01-05 16:14:07 -0600

    Implementación del Caso de Uso 8

--- app/src/main/java/com/example/prototipo3/MainActivity.java
+++ app/src/main/java/com/example/prototipo3/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.prototipo3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btn01 = this.findViewById(R.id.main_btn_01);
+
+        btn01.setOnClickListener(v -> {
+            Intent secondActivity = new Intent(MainActivity.this, SecondActivity.class);
+            startActivity(secondActivity);
+        });
+
+        Button btn02 = this.findViewById(R.id.main_btn_02);
+
+        btn02.setOnClickListener(v -> {});
     }
 }
