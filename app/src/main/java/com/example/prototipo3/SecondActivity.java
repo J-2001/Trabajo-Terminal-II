c6947648bc5c43a7b20341bfeeb775e6f61f7f92
J-2001 <roldangomezjuan01@gmail.com>
2024-05-06 00:04:37 -0600

    Solicitud de Extracción de Datos al Servidor para la Construcción del Reporte

--- app/src/main/java/com/example/prototipo3/SecondActivity.java
+++ app/src/main/java/com/example/prototipo3/SecondActivity.java
@@ -11,6 +11,8 @@ import java.io.ByteArrayOutputStream;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
 
 public class SecondActivity extends AppCompatActivity {
 
@@ -19,19 +21,49 @@ public class SecondActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
 
-        TextView tv = this.findViewById(R.id.second_tv_01);
+        TextView tv = this.findViewById(R.id.second_tv);
 
         new Thread(() -> {
             try {
                 Log.d("SecondActivity", "Obteniendo los tokens del servidor...");
-                String tokens = getTokens();
-                runOnUiThread(() -> tv.setText(tokens));
-
-                Log.d("SecondActivity", "Tokens obtenidos!");
+                int n = requestDevicesInfo();
+                runOnUiThread(() -> tv.setText(getString(R.string.second_tv_02, n)));
+                int limit = 10;
+                while (limit > 0) {
+                    Thread.sleep(1000);
+                    limit--;
+                }
             } catch (Exception e) {
                 Log.e("Error al obtener los tokens: ", e.toString());
             }
         }).start();
+    }
+
+    public int requestDevicesInfo() {
+        try {
+            URL url = new URL("https://trabajo-terminal-servidor.uc.r.appspot.com");
+            HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+
+            try {
+                urlConnection.setRequestProperty("Accion", "Info");
+                InputStream is = new BufferedInputStream(urlConnection.getInputStream());
+                ByteArrayOutputStream baos = new ByteArrayOutputStream();
+                byte[] buffer = new byte[1024];
+                int nRead;
+                while ((nRead = is.read(buffer)) != -1) {
+                    baos.write(buffer, 0, nRead);
+                }
+                String n = new String(baos.toByteArray(), StandardCharsets.UTF_8);
+                return Integer.parseInt(n);
+            } catch (Exception e) {
+                throw new Exception(e.getCause());
+            } finally {
+                urlConnection.disconnect();
+            }
+        } catch (Exception e) {
+            Log.e("Error - requestDevicesInfo(): ", e.toString());
+            return 0;
+        }
     }
 
     public String getTokens() {
